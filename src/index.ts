592c4e98e1dd41589184e4e70c4ea10da44cb4e9
Vasil Rimar <vrimar@thirdeyetech.ca>
2017-06-29 20:35:19 -0400

    Add source files

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import * as m from 'mithril';
+import 'normalize.css';
+
+const component = {
+	view() {
+		return m('h1', 'Mithril-webpack-typescript-boilerplate');
+	}
+};
+
+m.mount(document.getElementById('App'), component);
