0ffa6b4f4f7ee1d2359b4b6122007eb8592e6e61
Vasil Rimar <vrimar@thirdeyetech.ca>
2017-08-10 14:03:44 -0400

    Add indent linting and fix script

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ import * as m from 'mithril';
 import 'normalize.css';
 
 const component = {
-	view() {
-		return m('h1', 'Mithril-webpack-typescript-boilerplate');
-	}
+  view() {
+    return m('h1', 'Mithril-webpack-typescript-boilerplate');
+  }
 };
 
 m.mount(document.getElementById('App'), component);
