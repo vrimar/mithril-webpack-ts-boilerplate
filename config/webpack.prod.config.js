21c84b8e6a12140231d8da52cd713ecfaa1624d2
Vasil Rimar <vrimar@thirdeyetech.ca>
2017-10-04 21:33:26 -0400

    Update prod script

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -6,50 +6,48 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = function () {
-	return merge(baseConfig(), {
-		module: {
-			rules: [
-				{
-					test: /\.s?css$/,
-					use: ExtractTextPlugin.extract({
-						fallback: 'style-loader',
-						use: [
-							{ loader: 'css-loader', options: { minimize: true, importLoaders: 1 } },
-							{
-								loader: 'postcss-loader',
-								options: {
-									sourceMap: true,
-									plugins: () => [require('autoprefixer')]
-								}
-							},
-							'resolve-url-loader',
-							'sass-loader?sourceMap'
-						]
-					})
-				}
-			]
-		},
-		plugins: [
-			new webpack.optimize.UglifyJsPlugin({
-				beautify: false,
-				mangle: {
-					screw_ie8: true,
-					keep_fnames: true
-				},
-				compress: {
-					screw_ie8: true
-				},
-				comments: false
-			}),
-			new HtmlWebpackPlugin({
-				title: require('../package.json').name,
-				hash: true,
-				template: path.resolve(__dirname, '../src/index.template.ejs'),
-				favicon: path.resolve(__dirname, '../resources/logo.svg'),
-				hash: false
-			}),
-			new ExtractTextPlugin('css/app-[hash].css'),
-		],
-		devtool: 'nosources-source-map',
-	});
+  return merge(baseConfig(), {
+    module: {
+      rules: [
+        {
+          test: /\.s?css$/,
+          use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: [
+              { loader: 'css-loader', options: { minimize: true, importLoaders: 1 } },
+              {
+                loader: 'postcss-loader',
+                options: {
+                  sourceMap: true,
+                  plugins: () => [require('autoprefixer')]
+                }
+              },
+              'resolve-url-loader',
+              'sass-loader?sourceMap'
+            ]
+          })
+        }
+      ]
+    },
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin({
+        beautify: false,
+        mangle: {
+          screw_ie8: true,
+          keep_fnames: true
+        },
+        compress: {
+          screw_ie8: true
+        },
+        comments: false
+      }),
+      new HtmlWebpackPlugin({
+        title: require('../package.json').name,
+        template: path.resolve(__dirname, '../src/index.template.ejs'),
+        favicon: path.resolve(__dirname, '../resources/logo.svg')
+      }),
+      new ExtractTextPlugin('css/app-[hash].css'),
+    ],
+    devtool: 'nosources-source-map',
+  });
 }
